6b43d893e03877fcfd27687c61dd4cee63c06ed1
JoaoVLima <celu.joaolima@gmail.com>
2025-09-22 15:50:13 -0300

    NetBeans Start project <3

---
+++ src/main/java/com/limadeveloper/arvorequad/ArvoreQuad.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.limadeveloper.arvorequad;
+
+/**
+ *
+ * @author Lima
+ */
+public class ArvoreQuad {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
